f220784f43b91ec2bca6856d2de6f92b12caa483
a7medmabrok0 <am5424@fayoum.edu.eg>
2023-03-25 11:15:39 +0200

    assignment 1 finished

---
+++ lib/page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Assignment1 extends StatelessWidget {
+  const Assignment1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text(
+          'First Application',
+          style: TextStyle(),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 4)]),
+          child: const Center(
+            child: Text(
+              'Ahmed Mabrouk',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
